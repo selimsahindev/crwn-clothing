6bf331ec3e0cd263419162ea4c2dff315916dbfb
Selim Sahin <selimsahin.sns@gmail.com>
2020-11-25 01:07:52 +0000

    implemented firebase utils, included ability to store authenticated users into firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,7 +53,7 @@ class SignIn extends React.Component {
             required
           />
           <div className='buttons'>
-            <CustomButton type='submit'>Sign in</CustomButton>
+            <CustomButton type='submit'>SIGN IN</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,39 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDjlzbIsUXQ1P_LoRGu8jwonFOUy0fa1gA",
-  authDomain: "crwn-clothing-2d259.firebaseapp.com",
-  databaseURL: "https://crwn-clothing-2d259.firebaseio.com",
-  projectId: "crwn-clothing-2d259",
-  storageBucket: "crwn-clothing-2d259.appspot.com",
-  messagingSenderId: "927888578134",
-  appId: "1:927888578134:web:d48d4d0aa98049abc7cdf7"
+  apiKey: 'AIzaSyDjlzbIsUXQ1P_LoRGu8jwonFOUy0fa1gA',
+  authDomain: 'crwn-clothing-2d259.firebaseapp.com',
+  databaseURL: 'https://crwn-clothing-2d259.firebaseio.com',
+  projectId: 'crwn-clothing-2d259',
+  storageBucket: 'crwn-clothing-2d259.appspot.com',
+  messagingSenderId: '927888578134',
+  appId: '1:927888578134:web:d48d4d0aa98049abc7cdf7',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error while creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
