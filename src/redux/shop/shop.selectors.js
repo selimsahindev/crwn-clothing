ae04afe300279818f6dec01fc7a913009b733503
Selim Sahin <selimsahin.sns@gmail.com>
2020-12-14 08:32:34 +0000

    created collection page component and added styles to it

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,5 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash/memoize';
-import { memo } from 'react';
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 
 const selectShop = (state) => state.shop;
 
@@ -17,10 +8,14 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   )
 );
