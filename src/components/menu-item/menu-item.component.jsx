a8b193c751ebc08f186a7cefa404275a5ae66b32
Selim Sahin <selimsahin.sns@gmail.com>
2020-12-14 13:55:33 +0000

    minor visual issues fixed

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -13,7 +13,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
       }}
     />
     <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='title'>{title.toUpperCase()}</span>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -40,8 +40,8 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with email and password</span>
+        <h2 className='title'>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
