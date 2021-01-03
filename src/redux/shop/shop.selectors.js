9af5483b7bb1a2e1850de7d090c3192807119efb
Selim Sahin <selimsahin.sns@gmail.com>
2021-01-03 02:14:52 +0000

    implemented redux thunk and container pattern for collection page and collection overview component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -19,3 +19,13 @@ export const selectCollection = memoize((collectionUrlParam) =>
     collections ? collections[collectionUrlParam] : null
   )
 );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  (shop) => !!shop.collections
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middleWares = [];
+const middleWares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middleWares.push(logger);
